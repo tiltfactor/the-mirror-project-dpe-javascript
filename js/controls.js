b24e80dbb5b18b54828444902da59edcf216ddac
Gareth Solbeck <gsolbeck@gmail.com>
2017-10-18 17:37:16 -0400

    Add settings panel and width sliders
    These sliders allow the user to adjust the width of the page and the
    width of the poems.

---
+++ js/controls.js
@@ -0,0 +1,54 @@
+
+var settingsButton = document.getElementById('settings-button'),
+    settingsPanel = document.getElementById('settings-panel'),
+    fontSizeSlider = document.getElementById('font-size-slider'),
+    fontSizeOutput = document.getElementById('font-size-output'),
+    poemWidthSlider = document.getElementById('poem-width-slider'),
+    poemWidthOutput = document.getElementById('poem-width-output'),
+    bookWidthSlider = document.getElementById('book-width-slider'),
+    bookWidthOutput = document.getElementById('book-width-output'),
+
+    settingsVisible = false;
+
+settingsButton.addEventListener('click', function() {
+    settingsVisible = !settingsVisible;
+    settingsPanel.style.display = settingsVisible ? 'block' : 'none';
+});
+
+function initSlider(sliderEl, outputEl, attributes, valueFunc, changeFunc) {
+    Object.keys(attributes).forEach(function(key) {
+        sliderEl.setAttribute(key, this[key]);
+    }, attributes);
+    outputEl.value = valueFunc(sliderEl.value);
+    changeFunc(sliderEl.value);
+    sliderEl.addEventListener('input', function() {
+        var value = valueFunc(this.value);
+        outputEl.value = value;
+        changeFunc(this.value);
+    });
+}
+
+initSlider(poemWidthSlider, poemWidthOutput,
+    {
+        min : 0,
+        max : 100,
+        step : 1,
+        value : 80
+    },
+    (value) => value + '%',
+    function(value) {
+        document.querySelectorAll('.poem1, .poem2')
+                .forEach((el) => el.style.width = value + '%');
+    }
+);
+
+initSlider(bookWidthSlider, bookWidthOutput,
+    {
+        min : 0,
+        max : 100,
+        step : 1,
+        value : 80
+    },
+    (value) => value + '%',
+    (value) => document.querySelector('body').style.width = value + '%'
+);
